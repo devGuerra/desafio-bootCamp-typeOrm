a10700eaf6438ed051f07043f04aa7296f6b8eda
Roberto Guerra <prosaka@hotmail.com>
2021-01-28 21:29:34 -0300

    add cors and return category in transaction response

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -15,9 +15,9 @@ class Transaction {
   @Column('decimal')
   value: number;
 
-  @ManyToOne(() => Category)
+  @ManyToOne(() => Category, category => category.transaction, { eager: true })
   @JoinColumn({ name: 'category_id' })
-  category: Category
+  category: Category;
 
   @Column()
   category_id: string;
